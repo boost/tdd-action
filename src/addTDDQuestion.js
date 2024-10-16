992985228fa62eb3436030d90cad12ed490352b3
Daniela Lemow <danielalemow@Danielas-MacBook-Pro.local>
2024-10-16 16:28:23 +1300

    fix: Try refactoring workflow into actions

--- src/addTDDQuestion.js
+++
@@ -1,6 +0,0 @@
-github.rest.issues.createComment({
-  issue_number: context.issue.number,
-  owner: context.repo.owner,
-  repo: context.repo.repo,
-  body: "## Test Driven Development Feedback \n\n Was any Test-Driven Development (TDD) used while working on this pull request? \n\n - [ ] Yes \n - [ ] No \n - [ ] Doesn't apply \n\n Share your experience with TDD on this PR. Did you encounter any challenges or find it particularly helpful? \n\n *Insert feedback here*"
-});
